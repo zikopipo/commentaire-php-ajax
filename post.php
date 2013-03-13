6509e19328db13650b2756670a07ad6e864c4971
zikopipo <moi_ziko_10@hotmail.fr>
2013-03-13 13:46:30 +0100

    post

---
+++ post.php
@@ -0,0 +1,21 @@
+/*
+created by zakaria lfatmi
+facebook : http://www.facebook.com/zakaria.lfatmi
+email : zakarialfatmi@gmail.com
+http://www.zikohost.com
+
+*/
+<?php
+$connect = mysql_connect('localhost','root','') or die('error connecter');
+mysql_select_db('php') or die('error select');
+
+
+if(isset($_POST['message'])&&isset($_POST['username']))
+{
+  $username = mysql_real_escape_string($_POST['username']);
+	$message = mysql_real_escape_string($_POST['message']);
+
+	$query = mysql_query("INSERT INTO livredor(id,username,message) VALUES('','$username','$message')") or die('error de insetr');
+}
+
+?>
