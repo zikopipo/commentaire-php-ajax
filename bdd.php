d22840dd39e62b344c1ff800e2adccecacb6907a
zikopipo <moi_ziko_10@hotmail.fr>
2013-03-13 22:03:49 +0100

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -11,7 +11,7 @@ $user = 'root';
 $password = '';
 $database = 'test';
 $db = mysql_connect($host, $user, $password) or die('Error : ' . mysql_error());
-mysql_select_db('php') or die('bdd introuvable');
+mysql_select_db($database) or die('bdd introuvable');
 mysql_query('SET NAMES utf8');
 
 ?>
