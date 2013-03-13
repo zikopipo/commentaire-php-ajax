264c811e146d3afedf96430dbfed9105c89078a7
zikopipo <moi_ziko_10@hotmail.fr>
2013-03-13 22:04:52 +0100

    Update post.php

--- post.php
+++ post.php
@@ -14,7 +14,7 @@ if(isset($_POST['message'])&&isset($_POST['username']))
   $username = mysql_real_escape_string($_POST['username']);
 	$message = mysql_real_escape_string($_POST['message']);
 
-	$query = mysql_query("INSERT INTO livredor(id,username,message) VALUES('','$username','$message')") or die('error');
+	$query = mysql_query("INSERT INTO $database(id,username,message) VALUES('','$username','$message')") or die('error');
 }
 
 ?>
